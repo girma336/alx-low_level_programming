cd78384ad88151401e8fe126830320efa5271498
girma336 <girmatarekegn.gi@gmail.com>
2022-03-10 12:49:34 -0800

    all

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(){
-  puts("Programming is like building a multilingual puzzle");
+   
+ puts("\"programming is like building a multilingual puzzle");
+ 
   return 0;
 
 }
