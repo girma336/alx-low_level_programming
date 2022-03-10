78806525f3cdb9f228cb40eacb8624c92697ac14
girma336 <girmatarekegn.gi@gmail.com>
2022-03-10 13:54:02 -0800

    last commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main(void){
 	/* first programming in ala */
    
- puts("\"programming is like building a multilingual puzzle");
+ puts("\"programming is like building a multilingual puzzle\n");
  
   return 0;
 
