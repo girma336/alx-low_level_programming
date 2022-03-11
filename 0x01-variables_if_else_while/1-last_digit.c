79116ff492ab8e012519d2e1c6f2f8e3fc17f5e6
girma336 <girmatarekegn.gi@gmail.com>
2022-03-11 07:26:06 -0800

    last commit for

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d", n);
+printf("Last digit of %d ", n);
 n = n % 10;
 
-printf(" is %d", n); 
+printf("is %d", n); 
 
 if (n > 5)
 {
