0118022b647082135b70e43e38e0b6e741eef370
girma336 <girmatarekegn.gi@gmail.com>
2022-03-11 10:45:21 -0800

    improper commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-Entry point
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+char n = '0';
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+printf("\n");
+return (0);
+}
