e297b9e7780d8e1485aec9de7c6dde24699e4e93
girma336 <girmatarekegn.gi@gmail.com>
2022-03-11 08:03:30 -0800

    ner letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-Entry point
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+while(ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+printf("\n");
+return(0);
+}
