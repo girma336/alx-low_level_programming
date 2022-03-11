054ed2ecbb1a175cf7f0f0f97b0c150fe8df134b
girma336 <girmatarekegn.gi@gmail.com>
2022-03-11 12:43:36 -0800

    last

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-Entry point
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+char n = '0';
+while (n <= 'f')
+{
+putchar(n);
+n++;
+}
+printf("\n");
+return (0);
+}
