40874243e220d7a642c98f13880a73460b97c9b4
girma336 <girmatarekegn.gi@gmail.com>
2022-03-10 22:17:52 -0800

    next day

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main
- * return to zero
+ * Return :Alaways 0(Success)
  */
-int main(void){
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
