f213aaf3b229d110e939f76ec3c6e9ab6be3b985
girma336 <girmatarekegn.gi@gmail.com>
2022-03-11 15:57:30 -0800

    hello

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 int n, i;
-for(n = '0'; n <= '9'; n++)
+for (n = '0'; n <= '9'; n++)
 {
 for (i = '0'; i <= '9'; i++)
 {
 if (n < i)
 {
-putchar('n');
-putchar('i');
+putchar(n);
+putchar(i);
 if (n != '8' || (n == '8' && i != '9'))
 {
 putchar(',');
