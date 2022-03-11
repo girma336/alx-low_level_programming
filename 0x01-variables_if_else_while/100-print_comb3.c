937fb38d0eaafe008890c0dd0ade445e0003433b
girma336 <girmatarekegn.gi@gmail.com>
2022-03-11 15:47:50 -0800

    hello

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+int n, i;
+for(n = '0'; n <= '9'; n++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+if (n < i)
+{
+putchar('n');
+putchar('i');
+if (n != '8' || (n == '8' && i != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
