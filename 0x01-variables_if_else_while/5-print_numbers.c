e5fb3b4603fcf413def5ca37463997f072d7d6da
girma336 <girmatarekegn.gi@gmail.com>
2022-03-11 10:05:57 -0800

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-Entry points
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+printf("\n");
+return (0);
+}
