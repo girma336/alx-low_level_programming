0fc5c33a96cdfb2797dfa6a8e58b1b794ff42da3
girma336 <girmatarekegn.gi@gmail.com>
2022-03-15 19:48:54 +0300

    second assignment for today

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *print_alphabet
+ *print_alphabet- abcd
+ *Description: print lowercase letter
  *Return: Void .
  */
 void print_alphabet_x10(void)
 {
 char ch[26] = "abcdefghijklmnopqrstuvwxyz";
-int i,j;
+int i, j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 26; j++)
