c9a6a1b4b1efa24986479daa4c13d4dc8fbc9f33
girma336 <girmatarekegn.gi@gmail.com>
2022-03-15 19:39:48 +0300

    second assignment for today

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet
+ *Return: Void .
+ */
+void print_alphabet_x10(void)
+{
+char ch[26] = "abcdefghijklmnopqrstuvwxyz";
+int i,j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 26; j++)
+{
+_putchar(ch[j]);
+}
+_putchar('\n');
+}
+}
