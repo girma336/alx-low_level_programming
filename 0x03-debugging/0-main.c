9b3979a1cbee49b7ad1de93490253ba79820fb6e
girma336 <girmatarekegn.gi@gmail.com>
2022-03-16 18:47:48 +0300

    last commit fo debb

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *positive_or_negative
+ *REturn: if n is grater than 0 positive
+ *or equal to 0 zero
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+how ro debegging
