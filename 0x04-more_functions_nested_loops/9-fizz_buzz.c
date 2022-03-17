afd2b882bbac3426abbea2284a9af0076a60e0d5
girma336 <girmatarekegn.gi@gmail.com>
2022-03-17 22:50:09 +0300

    triangle

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main- Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char i, j;
+  for ( i = '0'; i <= '9'; i++)
+    {
+      for ( j = '0'; j <= '9'; j++)
+	{
+	  if (
