ad3e4fa1f5989ba426ace69ca7c223313648758f
girma336 <girmatarekegn.gi@gmail.com>
2022-03-17 23:21:45 +0300

    uppercase latter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *_isupper- Entry point
+ *_isupper- Entry point.
+ *@c: int.
  *Return:1 if c is uppercase
  *otherwise 0
  */
@@ -15,8 +16,8 @@ return (1);
 }
 else
 {
-return (0);
-}
+  return (0);
+ }
 ch++;
 }
 return (0);
