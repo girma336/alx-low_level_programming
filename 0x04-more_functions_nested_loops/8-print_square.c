76b3872ada8ef16012f107121b2813f2132f1e3d
girma336 <girmatarekegn.gi@gmail.com>
2022-03-17 21:40:10 +0300

    print squar of hash tag

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_square- size of squar
+ *Return: if size 0 or lessthan 0 newline
+ *else print #
+ */
+void print_square(int size)
+{
+int i, j;
+char ch = '#';
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
