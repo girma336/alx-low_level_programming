ef7b1e63ab2c914a8e7e212b1b9b8fbabed0810f
girma336 <girmatarekegn.gi@gmail.com>
2022-03-17 22:54:18 +0300

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,13 +10,13 @@ if (size <= 0)
 {
 _putchar('\n');
 }
-for(i = 1; i <= size; i++)
+for (i = 1; i <= size; i++)
 {
-for(j = i; j < size; j++)
+for (j = i; j < size; j++)
 {
 _putchar(' ');
 }
-for(j = 1; j <= i; j++)
+for (j = 1; j <= i; j++)
 {
 _putchar('#');
 }
