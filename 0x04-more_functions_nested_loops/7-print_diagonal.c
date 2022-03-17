aa82b290c8daadd341fd397ceeb054533ac7c424
girma336 <girmatarekegn.gi@gmail.com>
2022-03-17 20:50:17 +0300

    giagonal matrix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,30 @@
 #include "main.h"
 /**
- *print_diagonal- diagonal 
+ *print_diagonal- diagonal
  *@c: void.
  *Return: the diagonal of given input
  */
 void print_diagonal(int n)
 {
-  for (int i = 0; i <
+int ch = 92;
+int i, j;
+if (n <= 0)
+{
+_putchar('\n');
+}
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (i == j)
+{
+_putchar(ch);
+}
+else
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
