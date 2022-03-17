88eac8959f58507e6a27f54cfd137a8a8a12dcad
girma336 <girmatarekegn.gi@gmail.com>
2022-03-18 00:39:11 +0300

    negative or positive of num

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,13 @@
 #include "main.h"
-void positive_or_negative(int i)
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}
+    printf("%d\n", positive_or_negative());
+    return (0);
 }
