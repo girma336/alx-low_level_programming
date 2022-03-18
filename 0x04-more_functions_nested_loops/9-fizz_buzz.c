87e3b9010d432d0024fd4d10af9324ff1dae17de
girma336 <girmatarekegn.gi@gmail.com>
2022-03-19 01:53:38 +0300

    last commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,18 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz\t");
+printf("FizzBuzz");
 
 else if ((i % 3) == 0)
 
-printf("Fizz\t");
+printf("Fizz");
 
 else if ((i % 5) == 0)
 
-printf("Buzz\t");
+printf("Buzz");
 else
-printf("%d\t", i);
+printf("%d", i);
+printf(" ");
 }
 printf("\n");
 return (0);
