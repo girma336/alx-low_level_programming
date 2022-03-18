88a984c1a2981a6834279a04721731f029b32c7a
girma336 <girmatarekegn.gi@gmail.com>
2022-03-18 11:27:07 +0300

    negative or positive of num

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,8 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ *main-Entry point
+ *Return:Always 0 (Success)
+ */
 int _putchar(char c)
 {
-  printf("%c", c);
-  return (0);
+printf("%c", c);
+return (0);
 }
