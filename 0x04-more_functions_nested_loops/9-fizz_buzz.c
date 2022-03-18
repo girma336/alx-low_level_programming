2174011d3692a2ea7a7bdbee98e75a2feb465889
girma336 <girmatarekegn.gi@gmail.com>
2022-03-19 01:25:26 +0300

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,15 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (i % 15 == 0)
-printf("FizzBuzz ");
+printf("FizzBuzz\t");
 
 else if (i % 3 == 0)
 
-printf("Fizz ");
+printf("Fizz\t");
 
 else if (i % 5 == 0)
 
-printf("Buzz ");
+printf("Buzz\t");
 else
 printf("%d ", i);
 }
