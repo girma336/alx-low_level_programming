ce8c41982a93ef5eae16b74889a0b87f5f990ad7
girma336 <girmatarekegn.gi@gmail.com>
2022-03-21 15:22:56 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen- find lenght of the string
+ *@s:char.
+ *Return:Always 0 length.
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+return (0);
+}
