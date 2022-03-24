2960c2dfb6d2dd3bd2e60e13b27016e52c6f86d3
girma336 <girmatarekegn.gi@gmail.com>
2022-03-24 04:11:47 +0300

    last commit for reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,22 +12,13 @@ void reverse_array(int *a, int n)
 {
 int i;
 char temp;
-
 i = 0;
-while (i < n)
+while (i < n / 2)
 {
 temp = a[i];
-a[i] = a[n - i - 1];
-a[n - i - 1] = temp;
-if (i != 0)
-{
-printf(", ");
-}
-if (i < (n / 2))
-printf("%d", a[i]);
-else
-printf("%d", a[n - i - 1]);
+a[i] = a[n - 1 - i];
+
+a[n - 1 - i] = temp;
 i++;
 }
-printf("\n");
 }
