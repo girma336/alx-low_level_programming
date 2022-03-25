ead0c70aedb0499707e3cb155bc99ab331a47362
girma336 <girmatarekegn.gi@gmail.com>
2022-03-26 01:52:17 +0300

    last commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,23 @@
  *@s:char
  *Return:char
  */
-char *leet(char *)
+char *leet(char *s)
 {
+
 
 int i = 0;
 while (*(s + i) != '\0')
 {
+
 if (s[i] == 'a' || s[i]  == 'A')
 s[i] = '4';
-if (s[i] == 'e' || s[i]  == 'E')
+else if (s[i] == 'e' || s[i]  == 'E')
 s[i] = '3';
-if (s[i] == 'o' || s[i]  == 'O')
+else if (s[i] == 'o' || s[i]  == 'O')
 s[i] = '0';
-if (s[i] == 't' || s[i]  == 'T')
+else if (s[i] == 't' || s[i]  == 'T')
 s[i] = '7';
-if (s[i] == 'l' || s[i]  == 'L')
+else if (s[i] == 'l' || s[i]  == 'L')
 s[i] = '1';
 i++;
 }
