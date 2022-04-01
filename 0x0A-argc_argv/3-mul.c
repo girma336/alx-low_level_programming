e134e96d3b3a9a027c6233594fa32b2c543b4c35
girma336 <girmatarekegn.gi@gmail.com>
2022-04-01 16:54:19 +0300

    1-arrgs

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,12 +10,13 @@
 int main(int argc, char *argv[])
 {
 int  mul;
-if (argc > 1)
+if (argc > 3 || argc < 3)
 {
-mul = atoi(argv[1]) * atoi( argv[2]);
-printf("%d\n", mul);
-}
-else
 printf("Error\n");
+return (1);
+}
+mul = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", mul);
+
 return (0);
 }
