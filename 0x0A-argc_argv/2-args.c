d92018ae2e2bd5b5398c2927219ab6c1d85161ba
girma336 <girmatarekegn.gi@gmail.com>
2022-04-01 17:11:44 +0300

    my commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main-Entry point
+ *@argc:int
+ *@argv:char
+ *Return:int
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
+ 
