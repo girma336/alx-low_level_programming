48b160927fa5d25603c8c61169f532bfdde907d6
girma336 <girmatarekegn.gi@gmail.com>
2022-04-05 13:43:47 +0300

    argsrostring

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,6 +11,7 @@ char *argstostr(int ac, char **av)
 {
 char *str;
 int i, j, k, len;
+len = 0;
 if (ac == 0 || av == NULL)
 return (NULL);
 for (i = 0; i < ac; i++)
@@ -19,7 +20,7 @@ for (j = 0; av[i][j] != '\0'; j++)
 len++;
 len++;
 }
-str = malloc(sizeof(char) * (len));
+str = malloc(sizeof(char) * (len + 1));
 if (str == NULL)
 return (NULL);
 k = 0;
