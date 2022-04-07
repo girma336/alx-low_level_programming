914bfd0eba030c2b6719a47c9d3771a36a80869c
girma336 <girmatarekegn.gi@gmail.com>
2022-04-07 11:01:58 +0300

    concatinat the string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ *string_nconcat-print concatinat string
+ *@s1:char
+ *@s2:char
+ *@n:unsigned int
+ *Return:char
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *s3;
+unsigned int i = 0, j = 0, l1 = strlen(s1), l2 = strlen(s2);
+
+if (n < l2)
+s3 = malloc(sizeof(char) * (l1 * n + 1));
+else
+s3 = malloc(sizeof(char) * (l1 * l2 + 1));
+if (s3 == NULL)
+return (NULL);
+
+while (i < l1)
+{
+s3[i] = s1[i];
+i++;
+}
+while (i < l1)
+{
+s3[i] = s1[i];
+i++;
+}
+while (n < l2 && i < (l1 + n))
+
+s3[i++] = s2[j++];
+
+while (n >= l2 && i < (l1 + l2))
+s3[i++] = s2[j++];
+
+s3[i] = '\0';
+return (s3);
+}
