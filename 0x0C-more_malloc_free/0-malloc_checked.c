4ec34f3091b4bc089c375064c20216556844f1cc
girma336 <girmatarekegn.gi@gmail.com>
2022-04-07 10:05:35 +0300

    checked malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *malloc_checked-return a pointer to the memory
+ *@b:unsigned int
+ *Return:void
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+
+p = malloc(b);
+
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
+}
