deace84b6481a6072fdb049520822dc37eef4faa
girma336 <girmatarekegn.gi@gmail.com>
2022-04-08 11:05:55 +0300

    encode the srting

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *);
+char *rot13(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
