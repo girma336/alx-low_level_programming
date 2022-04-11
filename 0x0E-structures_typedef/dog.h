a27ce62612c0abad4b5f992eb64528d86189038c
girma336 <girmatarekegn.gi@gmail.com>
2022-04-11 16:00:35 +0300

    dog hederfile

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog- adog's basic info
+ *@name:char
+ *@age:float
+ *@owner:char
+ *stract:struct type
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
