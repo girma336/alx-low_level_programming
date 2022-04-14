1b6f05f0fb80b2dec313fe32c152907943e3cc32
girma336 <girmatarekegn.gi@gmail.com>
2022-04-14 13:27:58 +0300

    variadic sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *sum_them_all-print the sum of inputs
+ *@n:constant unsigned int
+ *Return:int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+va_list ap;
+int sum;
+unsigned int i;
+va_start(ap, n);
+sum = 0;
+
+if (n == 0)
+return (0);
+
+for (i = 0; i < n; i++)
+sum += va_arg(ap, int);
+
+va_end(ap);
+return (sum);
+}
