16dbd7c47edc38f364862aa7fb55f054080b3565
girma336 <girmatarekegn.gi@gmail.com>
2022-04-26 18:12:52 +0300

    find loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,23 +1,24 @@
 #include "lists.h"
 
 /**
- *print_listint_safe-print the safe way
- *@head:pointer of the first node
- *Return:size_t
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
 size_t count = 0;
-long int num;
-while (head != NULL)
+long int diff;
+while (head)
 {
-num = head - head->next;
+diff = head - head->next;
 count++;
 printf("[%p] %d\n", (void *)head, head->n);
-if (num > 0)
+if (diff > 0)
 head = head->next;
 else
-{
+{   
 printf("->[%p] %d\n", (void *)head->next, head->next->n);
 break;
 }
