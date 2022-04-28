da59f05da54925425ebfb7c54133342cc1f7fe4a
girma336 <girmatarekegn.gi@gmail.com>
2022-04-28 16:17:14 +0300

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_binary-print binary number
+ *@n:long int
+ *Return:void
+ */
+
+void print_binary(unsigned long int n)
+{
+int i; count = 0;
+unsigned long int new;
+for (i = 63; i >=0; i--)
+{
+new = n >> i;
+
+if (new & 1)
+{
+putchar('1');
+count++;
+}
+else if (count)
+putchar('0');
+}
+if (!count)
+putchar('0');
+}
