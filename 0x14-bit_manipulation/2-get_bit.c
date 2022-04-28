8b64767c4feabf2e3c8bec27910374c84ec530f6
girma336 <girmatarekegn.gi@gmail.com>
2022-04-28 16:41:38 +0300

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *get_bit-print bit
+ *@n:long int
+ *@index:int
+ *Return:int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+int bit_val;
+if (index > 63)
+return (-1);
+bit_val = (n >> index) & 1);
+return (bit_val);
+}
