bccee9e9c0754ed82adf68a2d5e92675ebc68487
girma336 <girmatarekegn.gi@gmail.com>
2022-04-29 12:23:38 +0300

    wildcmp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int wildcmp(char *s1, char *s2);
 int is_palindrome(char *s);
 int is_prime_number(int n);
