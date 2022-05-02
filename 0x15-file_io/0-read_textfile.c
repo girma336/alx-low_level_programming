cf2f32f3c43e404b2d651f23233e7911231af22a
girma336 <girmatarekegn.gi@gmail.com>
2022-05-02 15:39:00 +0300

    file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *read_textfile-create text file
+ *@filename:this is name of file
+ *@letters:size of letter
+ *Return:size_t
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd, s, t;
+char *buf;
+if (!filename)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+return (0);
+
+buf = malloc(sizeof(char) *letters);
+if (buf == NULL)
+return (0);
+s = read(fd, buf, letters);
+
+if (s < 0)
+{
+free(buf);
+return (0);
+}
+buf[s] = '\0';
+close(fd);
+
+t = write(STDOUT_FILENO, buf, s);
+if (t < 0)
+{
+free(buf);
+return (0);
+}
+free(buf);
+return (t);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *create_file-read the text file from the user
+ *@text_content:size of letter
+ *@filename:name of file
+ *Return:int
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, t, s =0;
+if (filename == NULL)
+return (-1);
+
+fd = open(filename, O_CREAT | O_TRUNC, 0600);
+if (fd < 0)
+return (-1);
+
+if (text_content[s])
+{
+while (text_content[s])
+s++;
+t = write(fd, text_content, s);
+if (t != s)
+return (-1);
+}
+close(fd);
+return (1);
+}
