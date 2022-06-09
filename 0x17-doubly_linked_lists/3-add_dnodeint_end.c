f17b76522a2c45c2ef7808303aa2f2c17124fb12
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-09 16:52:44 +0300

    add end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (new == NULL)
 		return (NULL);
-	cerrent = *head;
+	current = *head;
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
