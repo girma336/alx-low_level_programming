f9752acd1547ef750976d7f47ed23b638b020724
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-06-09 15:07:30 +0300

    fix betty for

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,17 +8,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t node = 0;
+	size_t node = 0;
 
-  if (h == NULL)
-    return (0);
-  while(h)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-      node++;
-    }
-  
-  return (node);
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node++;
+	}
+	return (node);
 }
-       
