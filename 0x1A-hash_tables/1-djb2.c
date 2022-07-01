3f6fea992f305df0d2ace82caca2cb0fc00b7f2c
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-07-01 14:22:56 +0300

    1-djd

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2-implement the djb2 algorithm
- * @str:input string
- *Return:the hash value
- * /
-
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
@@ -13,6 +13,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }
+
+
