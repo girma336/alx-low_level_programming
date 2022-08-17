a21872798534dd63dded300443036f477bfbce05
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-17 17:29:40 +0300

    is_land

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 """first line contains this element"""
 
+
+def new_water(grid, i, j):
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j - 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
+
+
 def island_perimeter(grid):
     """Return the perimeter of grid"""
     size = 0
 
-    for i in grid:
-        for j in i:
-            if j == 1:
-                size += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                size += new_water(grid, i, j)
 
-    return (size * 2) + 2
+    return (size)
